8882c2daee2d423c40dfeca18a82343cbc7ddc80
rafaela justino <rafaela.larissalj@gmail.com>
2022-07-07 17:01:43 -0300

    feat:  added get ranking function/ finish battle controller

--- src/controllers/battleController.ts
+++ src/controllers/battleController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as battleService from "../services/battleService.js";
 
 export async function battle(req: Request, res: Response) {
   const { firstUser, secondUser } = req.body;
@@ -6,4 +7,11 @@ export async function battle(req: Request, res: Response) {
   if (!firstUser || !secondUser) {
     res.sendStatus(422);
   }
+  const battleResult = await battleService.battle(firstUser, secondUser);
+  res.send(battleResult);
+}
+
+export async function getRanking(req: Request, res: Response) {
+  const ranking = await battleService.find();
+  res.send(ranking);
 }
