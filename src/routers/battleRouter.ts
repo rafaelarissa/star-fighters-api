89d3a7343a03a676bbfe091f78763b111ff3f7b9
rafaela justino <rafaela.larissalj@gmail.com>
2022-07-07 17:21:52 -0300

    chore: setting .gitignore

--- src/routers/battleRouter.ts
+++ src/routers/battleRouter.ts
@@ -3,5 +3,6 @@ import * as battleController from "../controllers/battleController.js";
 
 const battleRouter = Router();
 battleRouter.post("/battle", battleController.battle);
+battleRouter.get("/ranking", battleController.getRanking);
 
 export default battleRouter;
