9955305d1d8518d06ba0cd96af3b4bcd6b7eb810
rafaela justino <rafaela.larissalj@gmail.com>
2022-07-07 16:20:11 -0300

    feat: users must have a github account

---
+++ src/repositories/fighterRepository.ts
@@ -0,0 +1,35 @@
+import { connection } from "../database";
+
+export async function find() {
+  const result = await connection.query(
+    `
+      SELECT * 
+        FROM fighters 
+      ORDER BY wins DESC, draws DESC;
+    `
+  );
+  return result.rows;
+}
+
+export async function findByUsername(username: string) {
+  const result = await connection.query(
+    `
+    SELECT * FROM fighters WHERE username=$1
+    `,
+    [username]
+  );
+  return result.rows[0];
+}
+
+export async function insert(username: string) {
+  const result = await connection.query(
+    `
+    INSERT INTO fighters (username, wins, losses, draws) 
+    VALUES ($1, 0, 0, 0)
+    RETURNING id;
+  `,
+    [username]
+  );
+
+  return result.rows[0];
+}
--- src/services/battleService.ts
+++ src/services/battleService.ts
@@ -1,9 +1,25 @@
 import axios from "axios";
+import * as fighterRepository from "../repositories/fighterRepository.js";
 
-async function getFighterRepos(username: string) {
+export async function getFighterRepos(username: string) {
   const { data } = await axios.get(
     `https://api.github.com/users/${username}/repos`
   );
 
   return data;
+}
+
+export async function find() {
+  return fighterRepository.find();
+}
+
+async function getFighter(username: string) {
+  const fighter = await fighterRepository.findByUsername(username);
+
+  if (!fighter) {
+    const createdFighter = await fighterRepository.insert(username);
+    return { id: createdFighter.id, username, wins: 0, losses: 0, draws: 0 };
+  }
+
+  return fighter;
 }
